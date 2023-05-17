1b40252930a00706f9498e508fb2be05c6769009
samsarj <samsarj@gmail.com>
2023-05-17 13:40:35 +0100

    simplified js

---
+++ js/clock.js
@@ -0,0 +1,30 @@
+webcg.on("data", function (data) {
+  document.querySelector(".f0").innerHTML = data.f0
+    ? data.f0.text || data.f0
+    : "";
+  document.querySelector(".f1").innerHTML = data.f1
+    ? data.f1.text || data.f1
+    : "";
+});
+
+function pad(num) {
+  return ("0" + parseInt(num)).substr(-2);
+}
+
+function updateTime() {
+  var clock = document.querySelector(".clock");
+  var d = new Date();
+  clock.textContent =
+    pad(d.getHours()) + ":" + pad(d.getMinutes()) + ":" + pad(d.getSeconds());
+}
+
+webcg.on("play", function () {
+  const overlay = document.querySelector(".overlay");
+  overlay.style.opacity = 1;
+  updateTime();
+  setInterval(updateTime, 1000);
+});
+webcg.on("stop", function () {
+  const overlay = document.querySelector(".overlay");
+  overlay.style.opacity = 0;
+});
