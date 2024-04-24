4043a64d467e93ede1fc071ff2b28d02a8f447ad
Sam Sarjudeen <Sam Sarjudeen>
2024-04-24 14:26:19 +0100

    better spacing logic

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -23,15 +23,20 @@ webcg.on("data", function (data) {
       (key) => key.startsWith("f") && !isNaN(key.substring(1))
     ).length / 2;
 
+  const totalWidth = 80; // Use 80% of the viewport to place the divs
+  const baseStep = totalWidth / (numberOfPersons + 1);
+  const offset = (100 - totalWidth) / 2; // Centering offset for the whole group
+
   for (let index = 0; index < numberOfPersons; index++) {
     const personDiv = document.createElement("div");
     personDiv.style.position = "absolute";
     personDiv.style.bottom = "var(--margin)";
-    personDiv.style.transform = "translate(-50%,-50%)";
+    personDiv.style.width = "15vw";
+    personDiv.style.transform = "translate(-50%, 0)";
     personDiv.classList.add(`person${index + 1}`);
 
-    // Calculate dynamic horizontal position
-    const leftPercentage = (index + 0.5) * (100 / numberOfPersons);
+    // Calculate left position with offset to center them
+    const leftPercentage = offset + (index + 1) * baseStep;
     personDiv.style.left = `${leftPercentage}vw`;
 
     const nameDiv = document.createElement("div");
@@ -39,8 +44,12 @@ webcg.on("data", function (data) {
     nameDiv.innerHTML = data[`f${2 * index}`].text || data[`f${2 * index}`];
 
     const scoreDiv = document.createElement("div");
-    scoreDiv.classList.add(`line`, "line-lower", `f${2 * index + 1}`, `grad-light`);
-    scoreDiv.style.width = "auto";
+    scoreDiv.classList.add(
+      `line`,
+      "line-lower",
+      `f${2 * index + 1}`,
+      `grad-light`
+    );
     scoreDiv.style.fontSize = "8vh";
 
     scoreDiv.innerHTML =
